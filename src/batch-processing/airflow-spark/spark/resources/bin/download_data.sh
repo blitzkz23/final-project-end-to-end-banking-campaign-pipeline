9aca5d89ea5f62da51d7c273a650ef3a0a87ca09
blitzkz23 <bltzaldykazu@gmail.com>
2022-12-17 10:14:05 +0000

    feat: implemented download data dags

--- src/batch-processing/airflow-spark/spark/resources/bin/download_data.sh
+++ src/batch-processing/airflow-spark/spark/resources/bin/download_data.sh
@@ -1,4 +1,4 @@
-URL="https://drive.google.com/file/d/192goyWajSF7o9SR-gZ9E_6WIz43IEyVa/"
-LOCAL_PATH="/usr/local/spark/resources/data/"
-wget ${URL} -P ${LOCAL_PATH} 
+URL="https://drive.google.com/uc?export=download&id=192goyWajSF7o9SR-gZ9E_6WIz43IEyVa"
+LOCAL_PATH="/usr/local/spark/resources/data/bank-additional-full.csv.zip"
 
+wget --no-check-certificate ${URL} -O ${LOCAL_PATH} 
