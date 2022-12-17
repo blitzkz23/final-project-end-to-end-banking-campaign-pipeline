6b2b147aa3b5639809d29d47081bb20716cf0676
blitzkz23 <bltzaldykazu@gmail.com>
2022-12-17 09:31:42 +0000

    refactor: delete unused data

--- src/batch-processing/airflow-spark/spark/resources/bin/download_data.sh
+++ src/batch-processing/airflow-spark/spark/resources/bin/download_data.sh
@@ -1,4 +1,4 @@
-URL="https://drive.google.com/file/d/192goyWajSF7o9SR-gZ9E_6WIz43IEyVa/view"
+URL="https://drive.google.com/file/d/192goyWajSF7o9SR-gZ9E_6WIz43IEyVa/"
 LOCAL_PATH="/usr/local/spark/resources/data/"
 wget ${URL} -P ${LOCAL_PATH} 
 
